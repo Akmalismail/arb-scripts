8f609f8b63524fd20afbe2832bd2169ffad9f9eb
Muhammad Akmal Bin Ismail <akmal.ismail@tmrnd.com.my>
2024-08-19 12:15:14 +0800

    @@ metadata

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # .arb scripts
 
 Just some helper python scripts for `.arb` files
+
+## sort_arb.py
+
+Sorts `.arb` keys. Places @@ metadata at the top then sorts keys in alphabetical order with their @ metadata data next to them.
--- sort_arb.py
+++ sort_arb.py
@@ -1,39 +1,45 @@
 import json
 import os
 
+
 def sort_arb_file(input_file, output_file):
-  """Sorts an .arb file by key and writes it to a new file, preserving meta-data."""
+    """Sorts an .arb file by key and writes it to a new file, preserving meta-data."""
 
-  with open(input_file, 'r') as f:
-    data = json.load(f)
+    with open(input_file, 'r') as f:
+        data = json.load(f)
 
-  meta_data = [key for key in data.keys() if key.startswith('@')]
-  keys = [key for key in data.keys() if key not in meta_data]
+    meta_data = [key for key in data.keys() if key.startswith('@')]
+    keys = [key for key in data.keys() if key not in meta_data]
 
-  locale = [i for i in meta_data if i.startswith('@@')][0]
-  sorted_data = {
-    locale: data[locale]
-  }
+    sorted_data = {}
 
-  for key in sorted(keys):
-    sorted_data[key] = data[key]
-    meta = f'@{key}'
+    double_alias = [i for i in meta_data if i.startswith('@@')]
+    double_alias.sort()
 
-    if meta in meta_data:
-      sorted_data[meta] = data[meta]
+    for double_a in double_alias:
+        sorted_data[double_a] = data[double_a]
 
-  with open(output_file, 'w') as f:
-    json.dump(sorted_data, f, indent=2)
+    for key in sorted(keys):
+        sorted_data[key] = data[key]
+        meta = f'@{key}'
+
+        if meta in meta_data:
+            sorted_data[meta] = data[meta]
+
+    with open(output_file, 'w') as f:
+        json.dump(sorted_data, f, indent=2)
+
 
 def process_arb_files(directory):
-  """Processes all .arb files in the specified directory."""
+    """Processes all .arb files in the specified directory."""
 
-  for file in os.listdir(directory):
-    if file.startswith("app_") and file.endswith(".arb") and not file.endswith('_sorted.arb'):
-      input_file = os.path.join(directory, file)
-      output_file = os.path.join(directory, file[:-4] + "_sorted.arb")
-      sort_arb_file(input_file, output_file)
+    for file in os.listdir(directory):
+        if file.startswith("app_") and file.endswith(".arb") and not file.endswith('_sorted.arb'):
+            input_file = os.path.join(directory, file)
+            output_file = os.path.join(directory, file[:-4] + "_sorted.arb")
+            sort_arb_file(input_file, output_file)
+
 
 if __name__ == "__main__":
-  current_directory = os.getcwd()
-  process_arb_files(current_directory)
+    current_directory = os.getcwd()
+    process_arb_files(current_directory)
